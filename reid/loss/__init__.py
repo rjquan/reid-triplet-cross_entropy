92f1450c0c27fdca2aeb3e97e92d9b1883407463
rjquan <quanruij@gmail.com>
2018-05-21 21:55:59 +0800

    20180521

---
+++ reid/loss/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from .triplet import TripletLoss
+from .lsr import LSRLoss
+from .NewTripletLoss import NewTripletLoss
+
+__all__ = [
+    'TripletLoss',
+    'LSRLoss',
+    'NewTripletLoss',
+]
